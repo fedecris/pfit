4cd1b5485366919fb07b9c6affbfe6d25cece860
Federico Cristina <federicocristina@gmail.com>
2022-03-23 19:30:03 -0300

    Initial commit

---
+++ src/main/java/ml/pfit/PfitApplication.java
@@ -0,0 +1,13 @@
+package ml.pfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ml/pfit/PfitApplicationTests.java
@@ -0,0 +1,13 @@
+package ml.pfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
